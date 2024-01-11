2232826241294ed54fafb7e062f6a3ae4e68f7b0
Kevin Vicente <kcvicente@utpl.edu.ec>
2024-01-11 16:37:10 -0500

    Cambios

---
+++ EjemplosRecursividad/src/paquete003/Clase01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete003;
+
+/**
+ *
+ * @author KEVIN
+ */
+public class Clase01 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String[][] obtenerCadena() {
+        String[][] cadena = {{"Loja", "Cuenca", "Quito"}, {"Guayaquil", "Zamora",
+             "Machala"}};
+
+        return cadena;
+
+    }
+}
